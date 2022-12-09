ce507fa9d71a4f8ddf47e9cade00921dd618c803
OlehVakulchyk <4020405@gmail.com>
2022-12-09 21:50:31 +0200

    Update bithdays.py
    finaly

--- bithdays.py
+++ bithdays.py
@@ -1,44 +1,34 @@
-from datetime import datetime, timedelta
-
-
 def get_birthdays_per_week(users):  # дні народження наступного тижня
+
+    from datetime import datetime, timedelta
 
     bithdays_next_week = {0: ['Monday', []], 1: ['Tuesday', []], 2: ['Wednesday', []],
                           3: ['Thursday', []], 4: ['Friday',  []]}
+
     current_datetime = datetime.now()
 
-    current_date = current_datetime.date()
-
-    # залишилось до настунної суботи(int)
+    # залишилось до суботи(int) цього тижня(може бути -1)
     to_saturday = 5 - current_datetime.weekday()
 
-    this_saturday = current_datetime + timedelta(days=to_saturday)
-
+    this_saturday = current_datetime + \
+        timedelta(days=to_saturday)   # субота цього тижня(datetime)
+    # субота наступного тижня(datetime)
     next_saturday = this_saturday + timedelta(days=7)
+
     for di in users:
         bd = di['bithday']
+        # ДН в цьому році(datetime)
         bithday_this_year = datetime(current_datetime.year, bd.month, bd.day)
         if this_saturday.date() <= bithday_this_year.date() < next_saturday.date():
-            if bithday_this_year.weekday() > 4:
+            if bithday_this_year.weekday() > 4:  # додаємо ДН за вихідні на понеділок
                 bithdays_next_week[0][1].append(di['name'])
             else:
                 bithdays_next_week[bithday_this_year.weekday()][1].append(
-                    di['name'])
-    for value in bithdays_next_week.values():
+                    di['name'])  # сортуємо інші ДН
+
+    for value in bithdays_next_week.values():  # Роздруковуємо
         if value[1]:
             print(value[0].ljust(10) + ':', end=' ')
             for i in range(len(value[1]) - 1):
                 print(value[1][i], end=', ')
             print(value[1][len(value[1]) - 1])
-
-
-users = [{'name': 'Ol', 'bithday': datetime(1965, 12, 10)},
-         {'name': 'Ole', 'bithday': datetime(1965, 12, 11)},
-         {'name': 'Oleg', 'bithday': datetime(1976, 12, 12)},
-         {'name': 'Olegg', 'bithday': datetime(1965, 12, 15)},
-         {'name': 'Olegv', 'bithday': datetime(1986, 12, 14)},
-         {'name': 'Olegp', 'bithday': datetime(1965, 12, 15)},
-         {'name': 'Olegvp', 'bithday': datetime(2005, 12, 15)}
-         ]
-
-get_birthdays_per_week(users)
